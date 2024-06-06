cf645e36d2ccf6b6a986e5298412d5a074343584
Joaquin-Sanchez-Cerdan <jac.sanchez@alumno.etec.um.edu.ar>
2024-06-06 09:40:37 -0300

    Pruebas TDD (No funcionan aun)

--- src/persistance/db.ts
+++ src/persistance/db.ts
@@ -9,7 +9,7 @@ import "dotenv/config"
 export const AppDataSource = new DataSource({
     type: 'mysql',
     host: process.env.DATABASE_HOST,
-    port: 3306,
+    port: Number(process.env.DATABASE_PORT),
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
